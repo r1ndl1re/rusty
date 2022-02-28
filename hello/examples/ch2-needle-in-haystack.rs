6e97b2f07418150fbef75dedb410d1025edf2424
Keiichiro Nishimori <rendlire@gmail.com>
2022-03-01 03:14:23 +0900

    add ch2

---
+++ hello/examples/ch2-needle-in-haystack.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let needle = 2;
+    let haystack = [1, 1, 2, 5, 15, 52, 203, 877, 4140, 21147];
+
+    for item in &haystack {
+        if *item == needle {
+            println!("{}", item);
+        }
+    }
+}
