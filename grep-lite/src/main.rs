6764bcfea3a4f29b1faac05418a894081a601ad1
Keiichiro Nishimori <rendlire@gmail.com>
2022-04-01 02:31:37 +0900

    add grep-lite

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use regex::Regex;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 
 /// Searches for patterns
 #[derive(Parser, Debug)]
@@ -12,8 +12,18 @@ struct Args {
     pattern: String,
 
     /// input file
-    #[clap(short, long)]
+    #[clap(short, long, default_value = "-")]
     input: String,
+}
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
 }
 
 fn main() {
@@ -22,14 +32,14 @@ fn main() {
     let re = Regex::new(&pattern).unwrap();
 
     let input = args.input;
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
 
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
-        match re.find(&line) {
-            Some(_) => println!("{}", line),
-            None => (),
-        }
+    if input == "-" {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
